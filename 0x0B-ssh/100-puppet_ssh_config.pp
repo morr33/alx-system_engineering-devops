e6d88997af90271b6ab10e73c289b87581ff1c47
morr33 <gborieampomah@gmail.com>
2024-01-03 21:09:12 +0000

    made changes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 #Puppet script to create ssh config file
 
-file_line {
+file_line { 'Turn off passwd auth':
 	ensure	=> 'present',
 	path	=> '/etc/ssh/ssh_config',
 	line	=> '	PasswordAuthentication no',
 }
-file_line {
+file_line { 'Declare identify file':
 	ensure	=> 'present',
 	path	=> '/etc/ssh/ssh_config',
 	line	=> '	IdentifyFile ~/.ssh/school'
